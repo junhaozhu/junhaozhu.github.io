6264066c736701e60a1677aa4207ddca4a195632
zibber <coodd11009@gmail.com>
2016-07-06 20:01:07 +0000

    attemp 1

---
+++ script.js
@@ -0,0 +1,49 @@
+$(".tm_xl1").mouseenter(function() {
+       $(".tm_xl1").attr('src', 'images/xl1_hover_395*640.jpg');
+     });
+   $(".tm_xl1").mouseleave(function() {
+       $(".tm_xl1").attr('src', 'images/xl1_395*640.jpg');
+     });
+     
+$(".tm_windex").mouseenter(function() {
+       $(".tm_windex").attr('src', 'images/windex_hover_395*640.jpg');
+     });
+   $(".tm_windex").mouseleave(function() {
+       $(".tm_windex").attr('src', 'images/windex_395*640.jpg');
+     });
+$(".tm_wendys").mouseenter(function() {
+       $(".tm_wendys").attr('src', 'images/wendys_hover_395*640.jpg');
+     });
+   $(".tm_wendys").mouseleave(function() {
+       $(".tm_wendys").attr('src', 'images/wendys_395*640.jpg');
+     });
+$(".tm_breathe").mouseenter(function() {
+       $(".tm_breathe").attr('src', 'images/breathe_hover_395*640.jpg');
+     });
+   $(".tm_breathe").mouseleave(function() {
+       $(".tm_breathe").attr('src', 'images/breathe_395*640.jpg');
+     });
+$(".tm_id").mouseenter(function() {
+       $(".tm_id").attr('src', 'images/idthief_hover_395*640.jpg');
+     });
+   $(".tm_id").mouseleave(function() {
+       $(".tm_id").attr('src', 'images/idthief_395*640.jpg');
+     });
+$(".tm_lamer").mouseenter(function() {
+       $(".tm_lamer").attr('src', 'images/lamer_hover_395*640.jpg');
+     });
+   $(".tm_lamer").mouseleave(function() {
+       $(".tm_lamer").attr('src', 'images/lamer_395*640.jpg');
+     });
+$(".tm_graphicdesigns").mouseenter(function() {
+       $(".tm_graphicdesigns").attr('src', 'images/graphicdesigns_hover_395*640.jpg');
+     });
+   $(".tm_graphicdesigns").mouseleave(function() {
+       $(".tm_graphicdesigns").attr('src', 'images/graphicdesigns_395*640.jpg');
+     });
+$(".tm_webdesigns").mouseenter(function() {
+       $(".tm_webdesigns").attr('src', 'images/webdesigns_hover_395*640.jpg');
+     });
+   $(".tm_webdesigns").mouseleave(function() {
+       $(".tm_webdesigns").attr('src', 'images/webdesigns_395*640.jpg');
+     });
